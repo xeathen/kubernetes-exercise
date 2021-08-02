5730fce8421e4a325c559ae4ce960c1c0b0b0e25
ethan <xeathenx@gmail.com>
2021-08-02 17:10:28 +0800

    add service [pod-info-provider]

---
+++ pod-info-provider/src/main/java/provider/Application.java
@@ -0,0 +1,14 @@
+package provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ethan
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
