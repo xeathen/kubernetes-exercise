5d26e7d6b5c2bb37361d08d151a7798280fafb96
xeathen <xeathen@gmail.com>
2022-01-10 14:22:57 +0800

    DEV: Info Controller

--- pod-info-provider/src/main/java/provider/controller/InfoController.java
+++ pod-info-provider/src/main/java/provider/controller/InfoController.java
@@ -3,6 +3,7 @@ package provider.controller;
 import org.springframework.boot.env.RandomValuePropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.ServerResponse;
 
 import java.util.Random;
 
@@ -14,7 +15,7 @@ public class InfoController {
     public static final String HOST_NUMBER = String.valueOf(new Random().nextInt());
 
     @GetMapping(path = "/info/host-number")
-    public String getHostNumber(){
+    public String getHostNumber() {
         return "The host number is: " + HOST_NUMBER + "\n";
     }
 }
